74313411f9d5f589b21d1978b00db7732bbca537
Facundo Decena <facundo.decena@gmail.com>
2020-06-08 23:19:41 -0300

    Test cases for insertion

--- BST/binaryTree_test.go
+++ BST/binaryTree_test.go
@@ -1,1 +1,19 @@
 package BST
+
+import "testing"
+
+func TestBST_Insert(t *testing.T) {
+	for _, tc := range insertCases {
+		err := tc.tree.Insert(tc.element)
+		if tc.tree.Cursor.Apex.Index != tc.element.Index {
+			t.Fatalf("FAIL: %s\nExpected %#v\nActual %#v", tc.description, tc.tree.Cursor.Apex.Index, tc.element.Index)
+		}
+		if err != nil && !tc.err {
+			t.Fatalf("FAIL: %s\nNo error expected", tc.description)
+		}
+		if err == nil && tc.err {
+			t.Fatalf("FAIL: %s\nError expected", tc.description)
+		}
+		t.Logf("PASS: %s", tc.description)
+	}
+}
--- BST/cases_test.go
+++ BST/cases_test.go
@@ -1,1 +1,67 @@
 package BST
+
+var insertCases = []struct {
+	description string
+	element     Structure
+	tree        BST
+	err         bool
+}{
+	{
+		description: "insert into empty tree",
+		element:     Structure{Index: 5},
+		tree:        BST{},
+		err:         false,
+	},
+	{
+		description: "insert repeated element",
+		element:     Structure{Index: 5},
+		tree: BST{
+			root: &Node{
+				Apex:  Structure{Index: 5},
+				left:  nil,
+				right: nil,
+			},
+		},
+		err: true,
+	},
+	{
+		description: "insert smaller element",
+		element:     Structure{Index: 4},
+		tree: BST{
+			root: &Node{
+				Apex:  Structure{Index: 5},
+				left:  nil,
+				right: nil,
+			},
+		},
+		err: false,
+	},
+	{
+		description: "insert bigger element",
+		element:     Structure{Index: 6},
+		tree: BST{
+			root: &Node{
+				Apex:  Structure{Index: 5},
+				left:  nil,
+				right: nil,
+			},
+		},
+		err: false,
+	},
+	{
+		description: "insert bigger element",
+		element:     Structure{Index: 7},
+		tree: BST{
+			root: &Node{
+				Apex: Structure{Index: 5},
+				right: &Node{
+					Apex:  Structure{Index: 6},
+					left:  nil,
+					right: nil,
+				},
+				left: nil,
+			},
+		},
+		err: false,
+	},
+}
